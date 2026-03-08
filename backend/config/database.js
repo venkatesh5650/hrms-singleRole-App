f90c6dddd2f33231e422fad2a43f75ec2e8547b6
Karthan Venkatesh <karthanvenkateshvenkatesh@gmail.com>
2026-03-08 14:53:34 +0000

    added database url

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,25 +1,15 @@
 const { Sequelize } = require("sequelize")
 require("dotenv").config()
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT)
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
   }
-)
+})
 
 module.exports = sequelize
