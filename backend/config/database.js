91ab765cef66fc9083bf2b8b33f8fe6307693c03
Karthan Venkatesh <karthanvenkateshvenkatesh@gmail.com>
2026-03-08 16:15:17 +0000

    fixing postgress

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,8 @@ const { Sequelize } = require("sequelize")
 require("dotenv").config()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "mysql",
+  dialect: "postgres",
+  protocol: "postgres",
   logging: false,
   dialectOptions: {
     ssl: {
